55beef643c3a5b8de7a5634c1f808c4c8175e88b
Madhavan Kr <madhavan.kr@walmart.com>
2018-03-09 10:05:54 +0530

    Added json schema validation and spring-boot-devtools dependency

--- src/main/java/com/example/controllers/PassengerRequestHandler.java
+++ src/main/java/com/example/controllers/PassengerRequestHandler.java
@@ -3,11 +3,11 @@ package com.example.controllers;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,9 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.datamodel.PassengerCheckInRequest;
 import com.example.datamodel.PassengerCheckInResponse;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.fge.jackson.JsonLoader;
+import com.github.fge.jsonschema.core.exceptions.ProcessingException;
+import com.github.fge.jsonschema.main.JsonSchema;
+import com.github.fge.jsonschema.main.JsonSchemaFactory;
 
 @RestController
 public class PassengerRequestHandler {
+	
+	@Autowired
+	private ObjectMapper mapper;
+	
+	
 	
 	@GetMapping("/search/{flightNo}/{passengerName}")
 	public PassengerCheckInResponse getHandler(@PathVariable String flightNo,@PathVariable String passengerName) throws IOException
@@ -50,21 +61,78 @@ public class PassengerRequestHandler {
 	@PostMapping("/passengerInfo")
 	public ResponseEntity<Void> postHandler(@RequestBody PassengerCheckInRequest requestBody) throws IOException
 	{
-		File file = new File(requestBody.getFlightNo());
-		BufferedReader reader;
-		String existing="";
-		if(file.exists())
+		String requestJson =mapper.writeValueAsString(requestBody);
+		JsonNode requestDataNode = getJsonNode(requestJson);
+		System.out.println(requestJson);
+		JsonSchema requestSchemaNode = getSchemaNode(new File("requestSchema.json"));
+		if(requestDataNode==null || requestSchemaNode==null)
 		{
-			reader = new BufferedReader(new FileReader(requestBody.getFlightNo()));
-			String temp;
-			while((temp=reader.readLine())!=null)
-			{
-				existing+=temp;
-			}
+			System.out.println("one of the nodes was null");
+			return ResponseEntity.badRequest().build();
 		}
-		BufferedWriter writer = new BufferedWriter(new FileWriter(requestBody.getFlightNo()));
-		writer.write(existing+requestBody.getPassengerName()+"\n");
-		writer.close();
-		return ResponseEntity.ok().build();
+		else if(!isRequestValid(requestDataNode,requestSchemaNode))
+		{
+			System.out.println("validation failed");
+			return ResponseEntity.badRequest().build();
+		}
+		else 
+		{
+			System.out.println("everything is fine!!");
+			File file = new File(requestBody.getFlightNo());
+			BufferedReader reader;
+			String existing = "";
+			if (file.exists()) {
+				reader = new BufferedReader(new FileReader(requestBody.getFlightNo()));
+				String temp;
+				while ((temp = reader.readLine()) != null) {
+					existing += temp+"\n";
+				}
+			}
+			BufferedWriter writer = new BufferedWriter(new FileWriter(requestBody.getFlightNo()));
+			writer.write(existing  + requestBody.getPassengerName() );
+			writer.close();
+			return ResponseEntity.ok().build();
+		}
+	}
+	
+	private JsonNode getJsonNode(String jsonText)
+	{
+		try {
+			return JsonLoader.fromString(jsonText);
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			return null;
+		}
+	}
+	
+	private JsonNode getJsonNode(File jsonFile)
+	{
+		try {
+			return JsonLoader.fromFile(jsonFile);
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			return null;
+		}
+	}
+	
+	private JsonSchema getSchemaNode(File SchemaFile)
+	{
+		JsonSchemaFactory factory = JsonSchemaFactory.byDefault();
+		try {
+			return factory.getJsonSchema(getJsonNode(SchemaFile));
+		} catch (ProcessingException e) {
+			System.out.println(e.getMessage());
+			return null;
+		}
+	}
+	
+	private boolean isRequestValid(JsonNode data, JsonSchema schema)
+	{
+		try {
+			return schema.validate(data).isSuccess();
+		} catch (ProcessingException e) {
+			System.out.println(e.getMessage());
+			return false;
+		}
 	}
 }
